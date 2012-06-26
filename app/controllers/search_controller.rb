d06a77a9c9b1fbda5816bb2754a225f05109bbbd
Cristiano Nascimento <cnascimento@korewa.com.br>
2012-06-26 15:54:22 -0300

    ajustes controllers e layout  fancy box -pausando para mecher com cast

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -6,7 +6,7 @@ class SearchController < ApplicationController
   
   def new
     @Cidades = Provider.all(:group => "city")
-    @Categorias = Provider.all(:group => "state")
+    @Estados = Provider.all(:group => "state")
     @Atuacao = Provider.all(:group => "area_of_​​expertise")
     @Disponibilidade = Provider.all(:group => "availability")
     #@Estados = Reseller.all(:conditions => "active =  true", :group => "state", :order => "state ASC")
@@ -15,12 +15,12 @@ class SearchController < ApplicationController
   
   def results
     @Cidades = Provider.all(:group => "city")
-    @Categorias = Provider.all(:group => "state")
+    @Estados = Provider.all(:group => "state")
     @Atuacao = Provider.all(:group => "area_of_​​expertise")
     @Disponibilidade = Provider.all(:group => "availability")
     
     #Parameters: {"search"=>{"atuacao"=>"teste2", "categoria"=>"sp", "disponibilidade"=>"teste2"}, "utf8"=>"✓", "y"=>"26", "x"=>"62", "cidade"=>""}
     
-    @Resultados = Provider.all(:conditions => ['area_of_​​expertise = ? AND category = ? AND availability = ?', params[:search][:atuacao],params[:search][:categoria], params[:search][:disponibilidade]])
+    @Resultados = Provider.all(:conditions => ['area_of_​​expertise = ? AND state = ? AND availability = ?', params[:search][:atuacao],params[:search][:estado], params[:search][:disponibilidade]])
   end
 end
